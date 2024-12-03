f5674ee6450e06a4ab4afc16b36f63519587b487
Lesly <lesly97420@gmail.com>
2024-12-03 17:57:25 +0100

    CRUD add Bootstrap

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -4,10 +4,15 @@ namespace App\Form;
 
 use App\Entity\Categorie;
 use App\Entity\Produit;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 class ProduitType extends AbstractType
 {
@@ -15,8 +20,12 @@ class ProduitType extends AbstractType
     {
         $builder
             ->add('Nom')
-            ->add('Description')
-            ->add('Prix')
+            ->add('Description', TextareaType::class, [
+                'constraints' => [
+                    new Length(['min' => 25]),
+                ]
+            ])
+            ->add('Prix', MoneyType::class)
             /*->add('Date_creation', null, [
               'widget' => 'single_text',
            ])*/
@@ -24,6 +33,7 @@ class ProduitType extends AbstractType
                 'class' => Categorie::class,
                 'choice_label' => 'Nom',
             ])
+
         ;
     }
 
