542c3065a029bb033021eba8bbd657bee49f58d9
Lesly <lesly97420@gmail.com>
2024-12-04 00:29:24 +0100

    CRUD add API Platform (GET and POST tested)

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -5,7 +5,9 @@ namespace App\Entity;
 use App\Repository\ProduitRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
 class Produit
 {
